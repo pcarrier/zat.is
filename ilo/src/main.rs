a5064ad5c9cc8df7749770409c939b67bfbf9f99
Pierre Carrier <pc@rrier.ca>
2022-02-01 05:17:21 -0500

    ilo: rust UUID

--- ilo/src/main.rs
+++ ilo/src/main.rs
@@ -1,2 +1,7 @@
-fn main() {
+use uuid::Uuid;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let root_uuid = Uuid::new_v5(&Uuid::NAMESPACE_DNS, b"zat.is");
+    print!("{}", root_uuid);
+    Ok(())
 }
