2d033b32117f6c1b7a14f69476502253f9f83a42
Pierre Carrier <pc@rrier.ca>
2022-02-01 11:14:25 -0500

    uuid

--- ilo/src/main.rs
+++ ilo/src/main.rs
@@ -1,4 +1,14 @@
 use uuid::Uuid;
+use zerocopy::{
+    AsBytes, FromBytes, LittleEndian, U16, U32, U64, Unaligned, LayoutVerified, BigEndian,
+};
+
+#[derive(FromBytes, AsBytes, Unaligned)]
+#[repr(C)]
+struct UKey {
+    a: U64<BigEndian>,
+    b: U64<BigEndian>,
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let root_uuid = Uuid::new_v5(&Uuid::NAMESPACE_DNS, b"zat.is");
