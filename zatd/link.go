a7901e99eb89ceab853cb49c84b4a3cb7258e918
Pierre Carrier <pc@rrier.fr>
2025-06-26 13:48:33 +0200

    remove spaces

--- zatd/link.go
+++ zatd/link.go
@@ -3,8 +3,10 @@ package main
 import (
 	"crypto/sha256"
 	"fmt"
-	"github.com/valyala/fasthttp"
 	"net/url"
+	"strings"
+
+	"github.com/valyala/fasthttp"
 )
 
 func shorten(path string) string {
@@ -15,10 +17,12 @@ func shorten(path string) string {
 
 func serveLink(ctx *fasthttp.RequestCtx) {
 	ctx.Response.Header.Set("Cache-Control", "max-age=31536000")
-	target, err := url.PathUnescape(string(ctx.Request.Header.RequestURI()[1:]))
+	escaped := strings.TrimSpace(string(ctx.Request.Header.RequestURI()[1:]))
+	unescaped, err := url.PathUnescape(escaped)
 	if err != nil {
 		ctx.Error(err.Error(), fasthttp.StatusInternalServerError)
 	}
+	target := strings.TrimSpace(unescaped)
 	shortened := shorten(target)
 	err = insertUrlWhenAbsent(shortened, target, ctx)
 	if err != nil {
